50300f29b5b53c2e135028cd559c91a92d5b1745
lalit <lalit@sixfactors.ca>
2016-08-31 01:40:30 -0400

    Table view Created and pushed data

---
+++ Weather-R-7/DailyWeatherReportCell.swift
@@ -0,0 +1,24 @@
+//
+//  DailyWeatherReportCell.swift
+//  Weather-R-7
+//
+//  Created by Lalit on 2016-08-31.
+//  Copyright © 2016 Bagga. All rights reserved.
+//
+
+import UIKit
+
+class DailyWeatherReportCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
