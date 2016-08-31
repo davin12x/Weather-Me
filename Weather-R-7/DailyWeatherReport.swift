ed9d176deeba873a90c4ad63c51cee93cc0462b4
lalit <lalit@sixfactors.ca>
2016-08-30 23:53:27 -0400

    Project init ,MVC created , Report object created and receiving data

---
+++ Weather-R-7/DailyWeatherReport.swift
@@ -0,0 +1,9 @@
+//
+//  DailyWeatherReport.swift
+//  Weather-R-7
+//
+//  Created by Lalit on 2016-08-29.
+//  Copyright © 2016 Bagga. All rights reserved.
+//
+
+import Foundation
---
+++ Weather-R-7/Events.swift
@@ -0,0 +1,9 @@
+//
+//  Events.swift
+//  Weather-R-7
+//
+//  Created by Lalit on 2016-08-30.
+//  Copyright © 2016 Bagga. All rights reserved.
+//
+
+import Foundation
